1a8071cdec89bde8e3987e9b51c4d119aeac15e1
ShubhamRwt <shubhamrwt02@gmail.com>
2020-04-12 19:58:16 +0530

    Linked list

--- src/com/company/Lec22/LinkedList.java
+++ src/com/company/Lec22/LinkedList.java
@@ -96,6 +96,162 @@ public class LinkedList {
         return temp;
     }
 
+    public void insertbetween(int index, int value){
+
+        if(index==0){
+            insertfirst(value);
+            return;
+        }
+
+        if(index==size){
+            insertlast(value);
+            return;
+        }
+
+        Node prev = get(index-1);
+        Node node = new Node(value);
+        node.next=prev.next;
+        prev.next=node;
+        size++;
+    }
+
+    public int deleteatindex(int index){
+
+        if(index==0){
+            return deletefirst();
+        }
+
+        if(index==size-1){
+            return deletelast();
+        }
+
+        Node prev= get(index-1);
+        int temp= prev.next.value;
+
+        prev.next=prev.next.next;
+        size--;
+        return temp;
+    }
+
+    public void duplicates(){
+
+        Node temp= head;
+
+        while(temp.next!=null){
+
+            if(temp.value==temp.next.value){
+                temp.next=temp.next.next;
+                size--;
+            }
+            else{
+                temp=temp.next;
+            }
+        }
+        tail=temp;
+    }
+
+    public Node find(int value){
+
+        Node temp= head;
+
+        while(temp!=null){
+
+            if(temp.value==value){
+                return temp;
+            }
+        }
+        return null;
+    }
+
+    public void reverse(){
+        reverse(head);
+    }
+
+    private void reverse(Node node){
+
+        if(node==tail){
+            head=tail;
+            return;
+        }
+
+        reverse(node.next);
+
+        tail.next=node;
+        tail=node;
+        tail.next=null;
+    }
+
+    public LinkedList merge(LinkedList first, LinkedList second){
+
+        LinkedList list =new LinkedList();
+
+        Node f =first.head;
+        Node s= second.head;
+
+        while(f!=null && s!=null){
+
+            if(f.value<s.value){
+                list.insertlast(f.value);
+                f=f.next;
+            }
+            else{
+                list.insertlast(s.value);
+                s=s.next;
+            }
+        }
+
+        while(f!=null){
+            list.insertlast(f.value);
+            f=f.next;
+        }
+
+        while(s!=null){
+            list.insertlast(s.value);
+            s=s.next;
+        }
+
+        return list;
+    }
+
+    public Node mid(){
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public LinkedList mergesort(LinkedList list){
+
+        if(list.size==1){
+            return list;
+        }
+
+        Node mid= list.mid();
+
+        LinkedList first = new LinkedList();
+        first.head= list.head;
+        first.tail = mid;
+        first.size = (list.size+1)/2;
+
+        LinkedList second = new LinkedList();
+        second.head= mid.next;
+        second.tail= list.tail;
+        second.size= list.size/2;
+
+        mid.next = null;
+
+        first= mergesort(first);
+        second=mergesort(second);
+
+        return merge(first,second);
+    }
 
     class Node{
 
--- src/com/company/Lec22/ListClient.java
+++ src/com/company/Lec22/ListClient.java
@@ -5,16 +5,26 @@ public class ListClient {
     public static void main(String[] args) {
 
         LinkedList list = new LinkedList();
-        list.insertfirst(1);
-        list.insertfirst(3);
+        list.insertlast(3);
+        list.insertlast(4);
+        list.insertlast(2);
+        list.insertlast(1);
+//        list.insertfirst(3);
 //        list.insertfirst(2);
 //        list.insertlast(3);
 
+        list.display();
+        System.out.println();
 
+        LinkedList list1 = list.mergesort(list);
+
+        list1.display();
 //        System.out.println(list.deletefirst());
 //
 //        System.out.println(list.deletelast());
-        list.display();
+//        System.out.println(list.mid());
+
+
 
 
     }
